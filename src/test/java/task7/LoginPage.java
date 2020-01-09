85ffe987aa505bee9af6bfdb4d563bd7fdf3ae20
Bruss <Birukou.aliaksei@gmail.com>
2020-01-09 13:59:20 +0300

    Task7_withTimeOuts

--- src/test/java/task7/LoginPage.java
+++ src/test/java/task7/LoginPage.java
@@ -1,7 +1,9 @@
 package task7;
 
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,14 +11,21 @@ import java.util.concurrent.TimeUnit;
 
 public class LoginPage {
 
-    private static WebDriver driver;
+    protected static WebDriver driver;
 
-    public static void runWeb() {
-//        System.setProperty("webdriver.chrome.driver", "C:/projects/qa_course/driver/chromedriver.exe");
-//        driver = new ChromeDriver();
-//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//        driver.manage().window().maximize();
-//        driver.get("http://open-eshop.stqa.ru/oc-panel/auth/login");
-    }
+    @BeforeClass
+    public static void page() {
+        System.setProperty("webdriver.chrome.driver", "C:/projects/qa_course/driver/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        }
+
+//    @AfterClass
+//    public static void logOut(){
+//        driver.findElement(By.xpath("//*[@class='btn dropdown-toggle btn-success navbar-btn']")).click();
+//        driver.findElement(By.partialLinkText("Logout")).click();
+//    }
+
 
 }
--- src/test/java/task7/LoginPageTest.java
+++ src/test/java/task7/LoginPageTest.java
@@ -3,57 +3,88 @@ package task7;
 import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
-public class LoginPageTest extends Locators{
-    private static WebDriver driver;
-//    Locators loc = new Locators();
+import static org.junit.Assert.assertEquals;
 
-    @BeforeClass
-    public static void runWeb() {
-        System.setProperty("webdriver.chrome.driver", "C:/projects/qa_course/driver/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-        driver.get("http://open-eshop.stqa.ru/oc-panel/auth/login");
-    }
+public class LoginPageTest extends LoginPage{
+
+//    static WebDriverWait wait;
+    private By loginform;
+    private By logOutForm = By.xpath("//*[@class='btn dropdown-toggle btn-success navbar-btn']");
+    private By logOuteFormButton = By.partialLinkText("Logout");
+    private By emeilField = By.xpath("//*[@class='col-lg-9']//*[@class='well form-horizontal auth']//*[@name='email']");
+    private By passwordField = By.xpath("//*[@class='col-lg-9']//*[@class='well form-horizontal auth']//*[@name='password']");
+    private By logInButton = By.xpath("//*[@class='col-lg-9']//*[@class='well form-horizontal auth']//*[@type='submit']");
+    private By eShopButton = By.xpath("//*[@class='glyphicon glyphicon-th']");
+    private By selectCouponButton = By.xpath("//*[@title='Coupons']");
+    private By newCoupon = By.xpath("//*[@class='btn btn-primary pull-right']");
+    private By couponName = By.xpath("//*[@id='name']");
+    private By couponDiscount = By.xpath("//*[@id='discount_amount']");
+    private By couponDate = By.xpath("//*[@name='valid_date']");
+    private By couponNum = By.xpath("//*[@id='number_coupons']");
+    private By submitBtn = By.xpath("//*[@type='submit']");
+    private By submitBtnForSearchCoupon = By.xpath("//*[@class='btn']");
+    private By searchForm = By.xpath("//*[@name='name']");
+    private By delBtn = By.xpath("//*[@class='glyphicon glyphicon-trash']");
+    private By confirmBtn = By.xpath("//*[@class='confirm']");
+
+    private String email = "demo@open-eshop.com";
+    private String password = "demo";
+    String couponPageLink = "http://open-eshop.stqa.ru/oc-panel/coupon";
+    String afterLoginUrl = "http://open-eshop.stqa.ru/oc-panel";
+
+
     @Test
     public void login() {
+        driver.get("http://open-eshop.stqa.ru/oc-panel/auth/login");
         driver.findElement(emeilField).sendKeys(email);
         driver.findElement(passwordField).sendKeys(password);
         driver.findElement(logInButton).submit();
-        Assert.assertTrue(true);
+        Assert.assertEquals(afterLoginUrl, driver.getCurrentUrl());
     }
 
     @Test
-    public void couponCreator(){
+    public void couponCreator() throws InterruptedException {
         driver.findElement(eShopButton).click();
         driver.findElement(selectCouponButton).click();
         driver.findElement(newCoupon).click();
+        Thread.sleep(1000);
         driver.findElement(couponName).sendKeys("shaurma");
+        driver.findElement(couponDiscount).click();
         driver.findElement(couponDiscount).sendKeys("55");
-        driver.findElement(couponDate).sendKeys("1999-12-27");// добавить переменную текущая дата
+        driver.findElement(couponDate).sendKeys("2020-01-09");// добавить переменную текущая дата
         driver.findElement(couponNum).clear();
         driver.findElement(couponNum).sendKeys("666");
-        driver.findElement(submitBtn).submit();
+        driver.findElement(submitBtn).click();
+        Assert.assertEquals("Success", driver.findElement(By.xpath("//*[@class='alert-heading']")).getText());
+        Assert.assertEquals("shaurma", driver.findElement(By.xpath("//*[@class='table-responsive']//tbody/tr[1]/td[1]")).getText());
 
     }
+
     @Test
     public void couponSearch() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(1);
         driver.findElement(searchForm).click();
         driver.findElement(searchForm).sendKeys("shaurma");
-        driver.findElement(submitBtn).submit();
+        driver.findElement(submitBtnForSearchCoupon).click();
+        Thread.sleep(1000);
         driver.findElement(delBtn).click();
+        Thread.sleep(1000);
         driver.findElement(confirmBtn).click();
+        System.out.println(driver.findElement(By.xpath("//*[@class='table table-striped table-bordered']/tbody/tr[1]/td[1]")).getText());
+        Assert.assertEquals("shaurma",driver.findElement(By.xpath("//*[@class='table table-striped table-bordered']/tbody/tr[1]/td[1]")).getText());
 
     }
-    @Ignore
-    @AfterClass
-    public void logUut(){
-        driver.findElement(logOutForm).click();
-        driver.findElement(logOuteFormButton).click();
-    }
 
+//    @AfterClass
+//    public static void closeBrowser(){
+//        driver.quit();
+//    }
 }
